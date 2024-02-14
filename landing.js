68d0fa14b70960774a9155da2c04f9be4c0a48b2
chinxyy <148203993+chinxyy@users.noreply.github.com>
2024-02-14 12:27:18 +0800

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,9 @@
+// landing.js
+
+// landing.js
+
+document.getElementById('portfolio-button').addEventListener('click', function() {
+    window.location.href = 'index.html'; // Change 'index.html' to the path of your index.html file
+  });
+  
+  
